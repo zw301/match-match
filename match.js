65f5386cb18564b54d5aff30fcc4a7685ce08319
Ziyan Wang <zw301@nyu.edu>
2018-04-19 09:19:22 -0700

    Turn off the double tap zoom

--- match.js
+++ match.js
@@ -534,6 +534,15 @@ class Match {
 
 }
 
+// Turn off the double tap zoom
+let lastTouchEnd = 0;
+document.documentElement.addEventListener('touchend', function(event) {
+  let now = Date.now();
+  if(now - lastTouchEnd <= 300) {
+    event.preventDefault();
+  }
+  lastTouchEnd = now;
+}, false);
 
 let match;
 let playing = false;
